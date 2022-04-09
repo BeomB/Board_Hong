3e06f2361d04bfde0515505c5f7dbbfb54493766
Beom <qjarl1119@naver.com>
2022-04-09 12:28:58 +0900

    Initial commit

---
+++ src/main/java/com/board/beomBoard/BeomBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.beomBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeomBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeomBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/beomBoard/BeomBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.beomBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeomBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
